17dc371855fd3fa21843beeb9b3b696befd8a663
Leo Lien <gwolianglien@gmail.com>
2026-04-06 15:50:21 -0400

    refactor Navbar component and remove conditional visibility logic; simplify CSS for hover effect

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,8 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-  const { pathname } = useLocation();
-  const isHome =
-    pathname === '/leolien' ||
-    pathname === '/leolien/';
-
   return (
-    <nav
-      className={[
-        "navbar",
-        "navbar-expand-lg",
-        "navbar-light",
-        "navbar-hover",
-        isHome ? "navbar-always-visible" : null,
-      ]
-        .filter(Boolean)
-        .join(" ")}
-    >
+    <nav className="navbar navbar-expand-lg navbar-light navbar-hover">
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
